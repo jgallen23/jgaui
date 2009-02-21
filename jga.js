cb93cdf15ff71cdd955d1d75265a52bf949929ac
jga <jga@c4b45c5d-1ac9-45b3-ad79-2d58c4487e41>
2009-02-21 17:32:29 +0000

    added jquery

--- jga.js
+++ jga.js
@@ -5,5 +5,6 @@ var jgaWriteScript = function(script) {
 	document.write('<script type="text/javascript" src="' + JGALOCATION + "/" + script + '"></script>');
 }
 
+jgaWriteScript("jquery-1.3.1.min.js");
 jgaWriteScript("jga.core.js");
 jgaWriteScript("jga.ui.js");
