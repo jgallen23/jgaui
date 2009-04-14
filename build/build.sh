a04d575aef892a43f621f60e2f47cc8a5a4c7226
Greg Allen <greg@jga23.com>
2009-04-13 18:14:35 -0700

    moved to scripts directory

--- build/build.sh
+++ build/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cat ../src/jquery-1.3.1.min.js > ../lib/jga.js
-cat ../src/jquery-ui-core-1.7.min.js >> ../lib/jga.js
-cat ../src/jga.core.js >> ../lib/jga.js
-cat ../src/jga.ui.js >> ../lib/jga.js
+cat ../src/scripts/jquery-1.3.1.min.js > ../lib/scripts/jga.js
+cat ../src/scripts/jquery-ui-core-1.7.min.js >> ../lib/scripts/jga.js
+cat ../src/scripts/jga.core.js >> ../lib/scripts/jga.js
+cat ../src/scripts/jga.ui.js >> ../lib/scripts/jga.js
 
-cat ../src/jquery-1.3.1.min.js > ../lib/jga-min.js
-cat ../src/jquery-ui-core-1.7.min.js >> ../lib/jga-min.js
-python jsmin.py <../src/jga.core.js >> ../lib/jga-min.js
-python jsmin.py <../src/jga.ui.js >> ../lib/jga-min.js
+cat ../src/scripts/jquery-1.3.1.min.js > ../lib/scripts/jga-min.js
+cat ../src/scripts/jquery-ui-core-1.7.min.js >> ../lib/scripts/jga-min.js
+python jsmin.py <../src/scripts/jga.core.js >> ../lib/scripts/jga-min.js
+python jsmin.py <../src/scripts/jga.ui.js >> ../lib/scripts/jga-min.js
