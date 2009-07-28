f986fde67664f334aa627e5adbbea46e953a94a3
Greg Allen <greg@jga23.com>
2009-07-27 17:04:41 -0700

    pulled out textbox hint

--- src/scripts/jga.ui.js
+++ src/scripts/jga.ui.js
@@ -1,49 +1,4 @@
 
-(function($) { 
-
-	$.fn.textboxHint = function(hint, options) {
-		var opts = $.extend({}, $.fn.textboxHint.defaults, options);
-
-		var hideHint = function() {
-			this
-				.removeClass(opts.hintClass)
-				.val('');
-		};
-
-		var showHint = function() {
-			this
-				.addClass(opts.hintClass)
-				.val(hint);
-		};
-
-		var isPassword = function() {
-			if (this.attr("type").toLowerCase() == "password")
-				return true;
-			else
-				return false;
-		};
-
-		return this.each(function() {
-			var $this = $(this);
-			
-			$this.bind("focus", function() {
-				if ($this.val() == hint)
-					hideHint.call($this)
-			});
-			$this.bind("blur", function() {
-				if ($this.val() == "") 
-					showHint.call($this);
-			});
-
-			showHint.call($this);
-		});
-	};
-	$.fn.textboxHint.defaults = {
-		hintClass: 'Hint'
-	}
-})(jQuery);
-
-
 (function($) {
 		
 	$.fn.inlineEdit = function(options) {
