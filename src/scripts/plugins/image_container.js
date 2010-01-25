ce30e263400471f29f2857a39d0ecd73f9fdf2f4
Greg Allen <mail@jga23.com>
2010-01-25 14:26:36 -0800

    image container script

---
+++ src/scripts/plugins/image_container.js
@@ -0,0 +1,48 @@
+(function($) {
+	$.fn.imageContainer = function(options) {
+		var opts = $.extend($.fn.imageContainer.defaults, options);
+
+		var getOffsets = function(w, h) {
+			if (opts.align == "center") {
+				return [
+					(opts.w - w)/2,
+					(opts.h - h)/2
+				];
+			} else if (opts.align == "bottom") {
+				return [
+					(opts.w - w)/2,
+					(opts.h - h)
+				];
+			}
+		}
+
+		var resize = function() {
+			var $img = $(this);
+			
+			if (this.width >= this.height && this.width > opts.w) {
+				this.width = opts.w;
+			} else if (this.height > this.width && this.height > opts.h) {
+				this.height = opts.h;
+			}
+			var offsets = getOffsets(this.width, this.height);
+			$img.css({ marginTop: offsets[1], marginLeft: offsets[0] });
+		}
+
+		return this.each(function() {
+			var $this = $(this);
+			var $container = $this.wrap("<span/>").parent();
+			$container.css({ height: opts.h, width: opts.w, display: 'block' })
+			if ($this.width() == 0) {
+				$this.bind("load", resize);
+			} else {
+				resize.call(this);
+			}
+		});
+	}
+
+	$.fn.imageContainer.defaults = {
+		h: 200,
+		w: 200,
+		align: 'center'
+	}
+})(jQuery);
